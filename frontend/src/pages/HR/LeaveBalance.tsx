fdb0daf5e3dc2085fb8222e89492a2854eb013c1
Priyanshu Sahu <priyanshusahu085@gmail.com>
2025-09-10 11:12:25 +0530

    Leave Balance Max Value Upated

--- frontend/src/pages/HR/LeaveBalance.tsx
+++ frontend/src/pages/HR/LeaveBalance.tsx
@@ -348,11 +348,12 @@ const HRLeaveBalance: React.FC = () => {
 
               <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
                 <div>
-                  <Label htmlFor="el">Earned Leave</Label>
+                  <Label htmlFor="el">Earned Leave (Max: 75)</Label>
                   <Input
                     id="el"
                     type="number"
                     min="0"
+                    max="75"
                     step="0.5"
                     value={leaveBalances.el}
                     onChange={(e) => handleLeaveBalanceChange('el', e.target.value)}
@@ -360,11 +361,12 @@ const HRLeaveBalance: React.FC = () => {
                   />
                 </div>
                 <div>
-                  <Label htmlFor="sl">Sick Leave</Label>
+                  <Label htmlFor="sl">Sick Leave (Max: 15)</Label>
                   <Input
                     id="sl"
                     type="number"
                     min="0"
+                    max="15"
                     step="0.5"
                     value={leaveBalances.sl}
                     onChange={(e) => handleLeaveBalanceChange('sl', e.target.value)}
